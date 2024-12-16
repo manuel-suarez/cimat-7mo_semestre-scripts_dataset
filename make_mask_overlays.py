4ff9567dbcd898cc2eebbb033a99a866694033e6
Arturo Suárez <manuel.suarez@cimat.mx>
2024-12-16 14:35:07 -0600

    Using interpolation=none for display mask correctly

--- make_mask_overlays.py
+++ make_mask_overlays.py
@@ -1,19 +1,16 @@
 import os
 import rasterio
-import PIL
 import numpy as np
+from PIL import Image
 from skimage.io import imread
 from matplotlib import pyplot as plt
 
-PIL.Image.MAX_IMAGE_PIXELS = None
+Image.MAX_IMAGE_PIXELS = None
 
 home_dir = os.path.expanduser("~")
-data_dir = os.path.join(home_dir, "data")
-sentinel_dir = os.path.join(data_dir, "cimat", "dataset-sentinel")
-envisat_dir = os.path.join(data_dir, "cimat", "dataset-envisat")
+data_dir = os.path.join(home_dir, "data", "cimat", "dataset-cimat")
 
-print(os.listdir(os.path.join(sentinel_dir, "image_tiff")))
-print(os.listdir(os.path.join(envisat_dir, "image_tiff")))
+print(os.listdir(os.path.join(data_dir, "image_tiff")))
 
 os.makedirs("figures", exist_ok=True)
 
@@ -22,31 +19,53 @@ def plot_image_and_mask(basepath, filename):
     fname = filename.split(".")[0]
 
     image = rasterio.open(os.path.join(basepath, "image_tiff", fname + ".tif")).read(1)
-    label = imread(os.path.join(basepath, "mask_bin", fname + ".png"), as_gray=True)
-    mask = np.ma.masked_where(label == 0, label)
-    print(np.unique(label))
-    print(fname, image.shape, label.shape)
+    label = imread(
+        os.path.join(basepath, "mask_bin", fname + ".png"), as_gray=True
+    ).astype(np.float32)
+    print("Label array: ", np.unique(label), label.shape, label.dtype)
+    # Binarize
+    binary_image = np.where(label > 0, 1, 0)
+    print(fname, image.shape, label.shape, binary_image.shape)
+    print(image.dtype, label.dtype, binary_image.dtype)
+    # mask = np.ma.masked_values(label, 0.0)
 
     fig, ax = plt.subplots(1, 3, figsize=(16, 6))
-    ax[0].imshow(image, cmap="gray")
     ax[0].set_title("SAR origin")
-    ax[1].imshow(mask, cmap="gray")
+    im0 = ax[0].imshow(image, cmap="gray")
+    fig.colorbar(im0, ax=ax[0])
+
     ax[1].set_title("Segmentation label")
+    im1 = ax[1].imshow(label, cmap="gray", vmin=0.0, vmax=1.0, interpolation="none")
+    fig.colorbar(im1, ax=ax[1])
     # Masked overlay
-    ax[2].imshow(image, cmap="gray")
-    palette = plt.cm.viridis
-    # palette.set_bad(alpha=0)
-    ax[2].imshow(mask, cmap=palette, alpha=1.0, vmin=0.0, vmax=1.0)
     ax[2].set_title("Mask")
+    im2 = ax[2].imshow(
+        binary_image,
+        cmap="viridis",
+        alpha=1.0,
+        vmin=0.0,
+        vmax=1.0,
+        interpolation="none",
+    )
+    fig.colorbar(im2, ax=ax[2])
 
     fig.suptitle(fname)
     plt.savefig(os.path.join("figures", fname + ".png"))
     plt.close()
 
 
-# Sentinel
-for fname in os.listdir(os.path.join(sentinel_dir, "image_tiff")):
-    plot_image_and_mask(sentinel_dir, fname)
-# Envisat
-for fname in os.listdir(os.path.join(envisat_dir, "image_tiff")):
-    plot_image_and_mask(envisat_dir, fname)
+slurm_ntasks = os.getenv("SLURM_NTASKS")
+slurm_procid = os.getenv("SLURM_PROCID")
+slurm_task_pid = os.getenv("SLURM_TASK_PID")
+print("SLURM_NTASKS: ", slurm_ntasks)
+print("SLURM_PROCID: ", slurm_procid)
+print("SLURM_TASK_PID: ", slurm_task_pid)
+
+# Open image according to slurm proc id
+index = int(slurm_procid) - 1
+print("Index of image: ", index)
+fname = os.listdir(os.path.join(data_dir, "image_tiff"))[index]
+print("Image: ", fname)
+
+# Data
+plot_image_and_mask(data_dir, fname)
