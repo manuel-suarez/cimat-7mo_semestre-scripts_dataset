cfe1393da513430c495712e6393c31f60a7a58b7
Arturo Suárez <manuel.suarez@cimat.mx>
2024-12-16 12:51:28 -0600

    Check mask

--- make_mask_overlays.py
+++ make_mask_overlays.py
@@ -22,22 +22,24 @@ def plot_image_and_mask(basepath, filename):
     fname = filename.split(".")[0]
 
     image = rasterio.open(os.path.join(basepath, "image_tiff", fname + ".tif")).read(1)
-    label = imread(os.path.join(basepath, "mask_bin", fname + ".png"))
+    label = imread(os.path.join(basepath, "mask_bin", fname + ".png"), as_gray=True)
+    mask = np.ma.masked_where(label == 0, label)
+    print(np.unique(label))
     print(fname, image.shape, label.shape)
 
-    fig, ax = plt.subplots(1, 3, figsize=(16, 8))
+    fig, ax = plt.subplots(1, 3, figsize=(16, 6))
     ax[0].imshow(image, cmap="gray")
     ax[0].set_title("SAR origin")
-    ax[1].imshow(label, cmap="gray")
+    ax[1].imshow(mask, cmap="gray")
     ax[1].set_title("Segmentation label")
     # Masked overlay
     ax[2].imshow(image, cmap="gray")
     palette = plt.cm.viridis
-    palette.set_bad(alpha=0.0)
-    mask = np.ma.masked_where(label == 0.0, label)
-    ax[2].imshow(label, cmap=palette, alpha=0.8, vmin=0.0, vmax=1.0)
+    # palette.set_bad(alpha=0)
+    ax[2].imshow(mask, cmap=palette, alpha=1.0, vmin=0.0, vmax=1.0)
     ax[2].set_title("Mask")
 
+    fig.suptitle(fname)
     plt.savefig(os.path.join("figures", fname + ".png"))
     plt.close()
 
