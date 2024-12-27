5aacfc0ebb4485d2bb54b27adfc141a2b4020933
Arturo Suárez <manuel.suarez@cimat.mx>
2024-12-27 14:53:02 -0600

    Updates to segmentation

--- build_segmentation_patches.py
+++ build_segmentation_patches.py
@@ -11,9 +11,7 @@ from tqdm import tqdm
 home_path = os.path.expanduser("~")
 data_path = os.path.join(home_path, "data", "cimat")
 src_path = os.path.join(data_path, "dataset-cimat")
-dst_path = os.path.join(data_path, "dataset-cimat", "classification")
-not_oil_dir = "not_oil"
-oil_dir = "oil"
+dst_path = os.path.join(data_path, "dataset-cimat", "segmentation")
 # Initial configuration
 patch_size = 224
 
@@ -25,8 +23,6 @@ def patchify_image(
     img_dir,
     mask_dir,
     dst_path,
-    oil_dir,
-    not_oil_dir,
     img_name,
     patch_size,
     max_not_oil_patches=None,
@@ -36,8 +32,6 @@ def patchify_image(
         img_dir,
         mask_dir,
         dst_path,
-        oil_dir,
-        not_oil_dir,
         img_name,
         patch_size,
     )
@@ -49,7 +43,7 @@ def patchify_image(
     # Scale image between 0 and 1
     min_image = np.min(image)
     max_image = np.max(image)
-    image = (image - min_image) / (max_image - min_image)
+    image_scaled = (image - min_image) / (max_image - min_image)
 
     # Verifying that image and mask have the same shape
     image_height, image_width = image.shape
@@ -61,9 +55,10 @@ def patchify_image(
     count_x = int(image_width // patch_size) + 1
     count_y = int(image_height // patch_size) + 1
     invalid_patches = 0
-    oil_mask_patch = 0
-    not_oil_mask_patch = 0
     total_patches = 0
+    oil_patches = 0
+    full_oil_patches = 0
+    empty_oil_patches = 0
 
     for index, (j, i) in tqdm(
         enumerate(itertools.product(range(count_y), range(count_x)))
@@ -78,7 +73,10 @@ def patchify_image(
         if y + patch_size > image_height:
             y = image_height - patch_size - 1
 
+        # Original image patch
         image_patch = image[y : y + patch_size, x : x + patch_size]
+        # Scaled image patch
+        image_scaled_patch = image_scaled[y : y + patch_size, x : x + patch_size]
         total_patches = total_patches + 1
         min_image_patch = np.min(image_patch)
         max_image_patch = np.max(image_patch)
@@ -86,49 +84,48 @@ def patchify_image(
         if min_image_patch == 0 and max_image_patch == 0:
             invalid_patches = invalid_patches + 1
             continue
-        dst_img_name = img_name + f"_{index:04d}_train.tif"
+        dst_img_name = img_name + f"_{index:04d}_train"
         mask_patch = mask[y : y + patch_size, x : x + patch_size]
         min_mask_patch = np.min(mask_patch)
         max_mask_patch = np.max(mask_patch)
+        if min_mask_patch == 1 and max_mask_patch == 1:
+            # Full oil patch
+            full_oil_patches = full_oil_patches + 1
         if min_mask_patch == 0 and max_mask_patch == 0:
-            if max_not_oil_patches is not None:
-                if max_not_oil_patches == 0:
-                    continue
-                max_not_oil_patches = max_not_oil_patches - 1
-            # Not oil patch
-            not_oil_mask_patch = not_oil_mask_patch + 1
-            # Output on not oil patches directory (for classification problem)
-            imsave(
-                os.path.join(dst_path, not_oil_dir, dst_img_name),
-                image_patch,
-                check_contrast=False,
-            )
-        else:
-            # Oil patch
-            oil_mask_patch = oil_mask_patch + 1
-            # Output on oil patches directory (for classification problem)
-            imsave(
-                os.path.join(dst_path, oil_dir, dst_img_name),
-                image_patch,
-                check_contrast=False,
-            )
+            # Empty oil patch
+            empty_oil_patches = empty_oil_patches + 1
+            continue
+
+        # We are only saving patches with at least some content of oil
+        oil_patches = oil_patches + 1
+        imsave(
+            os.path.join(dst_path, "images", dst_img_name + ".tif"),
+            image_scaled_patch,
+            check_contrast=False,
+        )
+        imsave(
+            os.path.join(dst_path, "labels", dst_img_name + ".png"),
+            mask_patch,
+            check_contrast=False,
+        )
     print(
-        f"{img_name}, width, height: ({image_width}, {image_height}), total patches: {total_patches}, invalid_patches: {invalid_patches}, oil patches: {oil_mask_patch}, not oil patches: {not_oil_mask_patch}"
+        f"{img_name}, width, height: ({image_width}, {image_height}), total patches: {total_patches}, invalid_patches: {invalid_patches}, oil patches: {oil_patches}, full oil patches: {full_oil_patches}, empty oil patches: {empty_oil_patches}"
     )
     return (
         image_width,
         image_height,
         total_patches,
         invalid_patches,
-        oil_mask_patch,
-        not_oil_mask_patch,
+        oil_patches,
+        full_oil_patches,
+        empty_oil_patches,
     )
 
 
 # Create output directories
 os.makedirs(dst_path, exist_ok=True)
-os.makedirs(os.path.join(dst_path, oil_dir), exist_ok=True)
-os.makedirs(os.path.join(dst_path, not_oil_dir), exist_ok=True)
+os.makedirs(os.path.join(dst_path, "images"), exist_ok=True)
+os.makedirs(os.path.join(dst_path, "labels"), exist_ok=True)
 
 results = {
     "image": [],
@@ -137,21 +134,26 @@ results = {
     "total_patches": [],
     "invalid_patches": [],
     "oil_patches": [],
-    "not_oil_patches": [],
+    "full_oil_patches": [],
+    "empty_oil_patches": [],
 }
 for fname in os.listdir(os.path.join(src_path, "image_tiff")):
-    width, height, total_patches, invalid_patches, oil_patches, not_oil_patches = (
-        patchify_image(
-            src_path,
-            "image_tiff",
-            "mask_bin",
-            dst_path,
-            oil_dir,
-            not_oil_dir,
-            fname.split(".")[0],
-            patch_size,
-            120,
-        )
+    (
+        width,
+        height,
+        total_patches,
+        invalid_patches,
+        oil_patches,
+        full_oil_patches,
+        empty_oil_patches,
+    ) = patchify_image(
+        src_path,
+        "image_tiff",
+        "mask_bin",
+        dst_path,
+        fname.split(".")[0],
+        patch_size,
+        120,
     )
     results["image"].append(fname)
     results["width"].append(width)
@@ -159,7 +161,8 @@ for fname in os.listdir(os.path.join(src_path, "image_tiff")):
     results["total_patches"].append(total_patches)
     results["invalid_patches"].append(invalid_patches)
     results["oil_patches"].append(oil_patches)
-    results["not_oil_patches"].append(not_oil_patches)
+    results["full_oil_patches"].append(full_oil_patches)
+    results["empty_oil_patches"].append(empty_oil_patches)
 results_df = pd.DataFrame.from_dict(results)
-results_df.to_csv("results_classification.csv")
+results_df.to_csv("results_segmentation.csv")
 print("Done!")
