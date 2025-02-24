2ef8359c410843bf42857b4b007ed8f99f26af37
Arturo Suárez <manuel.suarez@cimat.mx>
2025-02-23 22:15:37 -0600

    Set relative paths to datasets

--- check_bin_channels.py
+++ check_bin_channels.py
@@ -4,7 +4,11 @@ import numpy as np
 from PIL import Image
 from skimage.io import imread
 
+base_path = os.path.expanduser("~")
+data_path = os.path.join(base_path, "data", "cimat", "dataset-cimat")
+mask_path = os.path.join(data_path, "mask_bin")
+
 Image.MAX_IMAGE_PIXELS = None
-for mask_name in os.listdir("mask_bin"):
-    mask = imread(os.path.join("mask_bin", mask_name))
+for mask_name in os.listdir(mask_path):
+    mask = imread(os.path.join(mask_path, mask_name))
     print(mask_name, mask.shape, mask.dtype, mask.min(), mask.max(), np.unique(mask))
--- check_png_channels.py
+++ check_png_channels.py
@@ -3,7 +3,11 @@ import os
 from PIL import Image
 from skimage.io import imread
 
+base_path = os.path.expanduser("~")
+data_path = os.path.join(base_path, "data", "cimat", "dataset-cimat")
+mask_path = os.path.join(data_path, "mask_png")
+
 Image.MAX_IMAGE_PIXELS = None
-for mask_name in os.listdir("mask_png"):
-    mask = imread(os.path.join("mask_png", mask_name))
+for mask_name in os.listdir(mask_path):
+    mask = imread(os.path.join(mask_path, mask_name))
     print(mask_name, mask.shape, mask.dtype, mask.min(), mask.max())
