43506aba0d3727f5d3af22d37fad17a9c3fea0eb
Arturo Suárez <manuel.suarez@cimat.mx>
2025-02-28 11:54:47 -0600

    Count and sum patches pixels

--- sum_patches_pixels.py
+++ sum_patches_pixels.py
@@ -20,16 +20,6 @@ patch_size = 224
 
 Image.MAX_IMAGE_PIXELS = None
 
-# Definimos el modelo U-Net con un backbone preentrenado (ResNet)
-slurm_array_task_id = os.getenv("SLURM_ARRAY_TASK_ID")
-slurm_ntasks = os.getenv("SLURM_NTASKS")
-slurm_procid = os.getenv("SLURM_PROCID")
-slurm_task_pid = os.getenv("SLURM_TASK_PID")
-print("SLURM_ARRAY_TASK_ID: ", slurm_array_task_id)
-print("SLURM_NTASKS: ", slurm_ntasks)
-print("SLURM_PROCID: ", slurm_procid)
-print("SLURM_TASK_PID: ", slurm_task_pid)
-
 
 def count_patch_pixels(
     src_path,
@@ -47,6 +37,9 @@ def count_patch_pixels(
         img_name,
         patch_size,
     )
+    mask_patches_dict = {"patch_name": [], "oil_pixels": [], "not_oil_pixels": []}
+    oil_pixels = 0
+    not_oil_pixels = 0
     # In this case we are opening both image and mask to patchify at the same time
     # considering that we are removing outside regions pixels (SAR image) and separating
     # oil from not oil spill patches
@@ -69,26 +62,10 @@ def count_patch_pixels(
     count_x = int(image_width // patch_size) + 1
     count_y = int(image_height // patch_size) + 1
 
-    total_patches = count_x * count_y
-    patches_per_task = total_patches // int(slurm_ntasks)
-    missing_patches_per_task = total_patches % int(slurm_ntasks)
-
-    patches_indexes = [
-        int(slurm_procid) * patches_per_task + index
-        for index in range(patches_per_task)
-    ]
-    if int(slurm_procid) < missing_patches_per_task:
-        additional_index = int(slurm_ntasks) * patches_per_task + int(slurm_procid)
-        patches_indexes.append(additional_index)
-
-    print("Patches indexes: ", patches_indexes)
-
     # Build patchex indexes to process considering the max patches, ntasks and task process id
     patches_positions = list(itertools.product(range(count_y), range(count_x)))
-    for patch_index in patches_indexes:
-        j, i = patches_positions[patch_index]
-
-        print("Processing patch index: ", patch_index, i, j)
+    for patch_index, (j, i) in enumerate(patches_positions):
+        print("Processing patch position: ", patch_index, i, j)
         # Get pixel positions for patch
         y = patch_size * j
         x = patch_size * i
@@ -99,34 +76,19 @@ def count_patch_pixels(
         if y + patch_size > image_height:
             y = image_height - patch_size - 1
 
-        # Original image patch
-        image_patch = image[y : y + patch_size, x : x + patch_size]
-        # Scaled image patch
-        image_scaled_patch = image_scaled[y : y + patch_size, x : x + patch_size]
-        total_patches = total_patches + 1
-        min_image_patch = np.min(image_patch)
-        max_image_patch = np.max(image_patch)
-        # We are checking if patch image values are 0, if so then continue next patch (we are in an invalid SAR image patch)
-        # if min_image_patch == 0 and max_image_patch == 0:
-        #    invalid_patches = invalid_patches + 1
-        #    continue
         dst_img_name = img_name + f"_{patch_index:04d}_train"
-        mask_patch = mask[y : y + patch_size, x : x + patch_size]
-
-        # Count pixels
-        oil_pixels = mask_patch[mask_patch == 1].sum()
-        not_oil_pixels = mask_patch[mask_patch == 0].sum()
-
-        # Save dataframe
-        mask_patch_dict = {
-            "patch_name": [dst_img_name],
-            "oil_pixels": [oil_pixels],
-            "not_oil_pixels": [not_oil_pixels],
-        }
-        mask_patch_df = pd.DataFrame.from_dict(mask_patch_dict)
-        mask_patch_df.to_csv(
+        mask_patch_df = pd.read_csv(
             os.path.join(dst_path, "counts", "patches", dst_img_name + ".csv")
         )
+        oil_pixels += mask_patch_df["oil_pixels"].iloc[0]
+        not_oil_pixels += mask_patch_df["not_oil_pixels"].iloc[0]
+        mask_patches_dict["patch_name"].append(mask_patch_df["patch_name"].iloc[0])
+        mask_patches_dict["oil_pixels"].append(mask_patch_df["oil_pixels"].iloc[0])
+        mask_patches_dict["not_oil_pixels"].append(
+            mask_patch_df["not_oil_pixels"].iloc[0]
+        )
+
+    return mask_patches_dict, oil_pixels, not_oil_pixels
 
 
 # Create output directories
@@ -135,15 +97,39 @@ os.makedirs(os.path.join(dst_path, "features", "origin"), exist_ok=True)
 os.makedirs(os.path.join(dst_path, "images"), exist_ok=True)
 os.makedirs(os.path.join(dst_path, "labels"), exist_ok=True)
 os.makedirs(os.path.join(dst_path, "figures"), exist_ok=True)
-os.makedirs(os.path.join(dst_path, "counts", "patches"), exist_ok=True)
+os.makedirs(os.path.join(dst_path, "counts", "images"), exist_ok=True)
+os.makedirs(os.path.join(dst_path, "counts", "totals"), exist_ok=True)
 
-fname = os.listdir(os.path.join(src_path, "image_norm"))[int(slurm_array_task_id) - 1]
-count_patch_pixels(
-    src_path,
-    "image_norm",
-    "mask_bin",
-    dst_path,
-    fname.split(".")[0],
-    patch_size,
-)
+mask_images_dict = {"image_name": [], "oil_pixels": [], "not_oil_pixels": []}
+total_oil_pixels = 0
+total_not_oil_pixels = 0
+for fname in os.listdir(os.path.join(src_path, "image_norm")):
+    image_patches_dict, image_oil_pixels, image_not_oil_pixels = count_patch_pixels(
+        src_path,
+        "image_norm",
+        "mask_bin",
+        dst_path,
+        fname.split(".")[0],
+        patch_size,
+    )
+    # Save image patches CSV
+    image_patches_df = pd.DataFrame.from_dict(image_patches_dict)
+    image_patches_df.to_csv(
+        os.path.join(dst_path, "counts", "images", fname.split(".")[0] + ".csv")
+    )
+    # Accumulate images CSV
+    mask_images_dict["image_name"].append(fname)
+    mask_images_dict["oil_pixels"].append(image_oil_pixels)
+    mask_images_dict["not_oil_pixels"].append(image_not_oil_pixels)
+    # Total CSV
+    total_oil_pixels += image_oil_pixels
+    total_not_oil_pixels += image_not_oil_pixels
+
+# Save totals
+mask_totals_dict = {
+    "oil_pixels": [total_oil_pixels],
+    "not_oil_pixels": [total_not_oil_pixels],
+}
+mask_totals_df = pd.DataFrame.from_dict(mask_totals_dict)
+mask_totals_df.to_csv(os.path.join(dst_path, "counts", "totals", "total_count.csv"))
 print("Done!")
